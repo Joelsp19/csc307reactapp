1e621fa9d948b25b4c46125be67e229e124a512a
Joel Puthankalam <jputhank@calpoly.edu>
2023-10-06 00:47:29 -0700

    implemented deleteUserbyID and query for namejob

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,6 +38,11 @@ const findUserByName = (name) => {
         .filter( (user) => user['name'] === name); 
 }
 
+const findUserByJob = (job) => { 
+    return users['users_list']
+        .filter( (user) => user['job'] === job); 
+}
+
 const findUserById = (id) =>
     users['users_list']
         .find( (user) => user['id'] === id);
@@ -47,30 +52,62 @@ const addUser = (user) => {
     return user;
 }    
 
+const deleteUser = (user) => {
+    users['users_list'].splice(users['users_list'].indexOf(user));
+    return user;
+}
+
+
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+//gets the user by name or by job
 app.get('/users', (req, res) => {
     const name = req.query.name;
+    const job = req.query.job;
+    let name_list = users['users_list'];
+    let job_list = users['users_list'];
+
     if (name != undefined){
-        let result = findUserByName(name);
-        result = {users_list: result};
-        res.send(result);
+        name_list = findUserByName(name);
     }
-    else{
-        res.send(users);
+    if (job != undefined){
+        job_list = findUserByJob(job);
     }
+
+    //joins the two lists together --> gives one list with same values in both
+    let fin_list = name_list.filter((user) => job_list.includes(user)) 
+    let fin_result = {users_list: fin_list}
+    res.send(fin_result)
+
+    
+
 });
 
+//adds the user
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
     res.send();
 });
 
+//deletes the user
+app.delete('/users/:id', (req,res) => {
+    const idToDelete = req.params['id'];
+    let result = findUserById(idToDelete);
+    if (result === undefined){
+        res.status(404).send('Resource not found.');
+    } else {
+        deleteUser(result)
+        res.send()
+    }
+})
+
+//gets the user by id
 app.get('/users/:id', (req, res) => {
     const id = req.params['id']; //or req.params.id
     let result = findUserById(id);
